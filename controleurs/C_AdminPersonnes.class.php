2cb04d359e1e7294dff7229c6102619d25e2d5b7
btssio <btssio@btssio-VirtualBox>
2014-09-25 16:42:31 +0200

    pour maxime

--- controleurs/C_AdminPersonnes.class.php
+++ controleurs/C_AdminPersonnes.class.php
@@ -36,51 +36,36 @@ class C_AdminPersonnes extends C_ControleurGenerique {
     function validationcreerPersonne(){
         /*Modification, instanciation de la vue */
         $this->vue = new V_Vue("../vues/templates/template.inc.php") ;
-        /*fin modif*/
-        $this->vue->titreVue = "Validation cr&eacute;ation de l'utilisateur";
+        $daoPers = new M_DaoPersonne();
+        $daoPers->connecter();
        
-        $utilisateur = new M_DaoLesDonnesCreationUtilisateur();
-        // préparer la liste des paramètres
-        $lesParametres = array();
+       
+    
+      $id= $_POST['id'] ;
+      $option= $_POST['option'] ;
+      $role= $_POST['role'] ;
+      $civilite= $_POST['civilite'] ;
+      $nom= $_POST['nom'] ;
+      $prenom= $_POST['prenom'] ;
+      $tel= $_POST['tel'] ;
+      $portable= $_POST['telP'] ;
+      $mail= $_POST['mail'] ;
+      $etudes= $_POST['etudes'] ;
+      $formation= $_POST['formation'] ;
+      $login= $_POST['id'] ;
+      $mdp= $_POST['mdp'] ;
       
-        $lesLogin = new M_ListeLogin();
-        $countLog="";
-        $countLog= $lesLogin->getCount($_POST["login"]);
-        //$this->vue->ListeLogin = $lesLogin->getCountLogin($_POST["login"]);
-        $msg='';    
-        //vérifie si le login est présent dans la base de donnée si il ne l'est pas l'utilisateur est créé
-       
-      if($countLog->NB=="0"){
+       $newRole=New M_DaoRole($role, null,null) ;
+       $role = $newRole -> objetVersEnregistrement($role) ;
+       var_dump($role) ; die() ;
+
+       $pers = new M_Personne($id,$option,$role,$civilite,$nom,$prenom,$tel,$mail,$portable,$etudes,$formation,$login,$mdp);
+        //$pers=$daoPers->objetVersEnregistrement($pers);
+         //$daoPers ->insert($pers) ;
+        var_dump($pers) ;
+        die() ;
+ 
          
-        $lesParametres[0] = $utilisateur->getId('IDSPECIALITE', 'SPECIALITE', 'IDSPECIALITE', $_POST["option"]);
-        
-        $lesParametres[1] = $utilisateur->getId('IDROLE', 'ROLE', 'LIBELLE', $_POST["role"]);
-        $lesParametres[2] = $_POST["civilite"];  
-        $lesParametres[3] = $_POST["nom"];
-        $lesParametres[4] = $_POST["prenom"];
-        $lesParametres[5] = $_POST["tel"];
-        $lesParametres[6] = $_POST["mail"];
-        $lesParametres[7] = $_POST["telP"];
-        
-        $lesParametres[8] = $_POST["etudes"];
-        $lesParametres[9] = $_POST["formation"];
-        $lesParametres[10] = $_POST["entreprise1"];
-        
-        $lesParametres[11] = $_POST["login"];
-        $lesParametres[12] = sha1($_POST["mdp"]);
-          
-        $ok = $utilisateur->insert($lesParametres);
-        var_dump($lesParametres) ;
-      }else{
-          $msg=' Login déjà utilisé';
-          $ok=0;
-      }
-      
-        if ($ok) {
-            $this->vue->message = "Utilisateur cr&eacute;&eacute;";
-        } else {
-            $this->vue->message = "Echec de l ajout de l utilisateur".$msg;
-        }
         $this->vue->afficher();
     }
     
